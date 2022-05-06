f2e0fa827c74b51ba6043ef42d9130d9d44e68d0
AlexeyVasilev0101 <101921598+AlexeyVasilev0101@users.noreply.github.com>
2022-05-06 14:55:42 +0300

    EX8

---
+++ EX8OOP/EX8OOP.cpp
@@ -0,0 +1,13 @@
+﻿#include "Source.h"
+
+int main()
+{
+    Student Alexey;
+    cin >> Alexey;
+    cout << Alexey;
+    cout << endl;
+    Student Maksim;
+    cin >> Maksim;
+    Alexey = Maksim;
+    cout << Alexey << endl;
+}
